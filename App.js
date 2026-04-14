20ce93fcbb562eaa1703e17f92c6d74e28976db8
Ms_Njuguna <patricianjuguna084@gmail.com>
2026-04-15 00:03:29 +0300

    feat: build first screen(dashboard)

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import "./global.css"
-import { Text, View } from "react-native";
- 
+import { View, Text } from "react-native";
+
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to flockwise!
+    <View className="flex-1 bg-green-50 items-center justify-center">
+      <Text className="text-2xl font-bold">
+        🐔 flockwise Dashboard
+      </Text>
+
+      <Text className="mt-2 text-gray-600">
+        Farm smarter. Make profit.
       </Text>
     </View>
   );
