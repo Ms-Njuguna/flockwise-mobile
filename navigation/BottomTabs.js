48b2d36676d35ec330270ff75c83a7c03d63eadb
Ms_Njuguna <patricianjuguna084@gmail.com>
2026-04-15 14:45:03 +0300

    feat: setup bottom tab navigation structure

---
+++ navigation/BottomTabs.js
@@ -0,0 +1,20 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import DashboardScreen from "../screens/DashboardScreen";
+import FlockScreen from "../screens/FlockScreen";
+import RecordScreen from "../screens/RecordScreen";
+import FeedScreen from "../screens/FeedScreen";
+import InsightsScreen from "../screens/InsightsScreen";
+
+const Tab = createBottomTabNavigator();
+
+export default function BottomTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Dashboard" component={DashboardScreen} />
+      <Tab.Screen name="Flocks" component={FlockScreen} />
+      <Tab.Screen name="Records" component={RecordScreen} />
+      <Tab.Screen name="Feed" component={FeedScreen} />
+      <Tab.Screen name="Insights" component={InsightsScreen} />
+    </Tab.Navigator>
+  );
+}
