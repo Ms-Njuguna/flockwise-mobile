2931208100e50e66d6db9cec5c691c9a7cb6d2d4
Ms_Njuguna <patricianjuguna084@gmail.com>
2026-04-15 14:57:26 +0300

    feat: implement dashboard UI with profit and stats cards

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
+import "./global.css";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabs from "./navigation/BottomTabs";
-import "./global.css";
 
 export default function App() {
   return (
--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -2,8 +2,36 @@ import { View, Text } from "react-native";
 
 export default function DashboardScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-green-50">
-      <Text className="text-xl font-bold">Dashboard</Text>
+    <View className="flex-1 bg-green-50 p-4">
+
+      <Text className="text-2xl font-bold mb-4">
+        🐔 FlockWise
+      </Text>
+
+      <View className="bg-white p-4 rounded-xl shadow mb-4">
+        <Text className="text-gray-500">Today's Profit</Text>
+        <Text className="text-2xl font-bold text-green-600">
+          KES 120
+        </Text>
+      </View>
+
+      <View className="flex-row justify-between">
+        <View className="bg-white p-4 rounded-xl w-[30%]">
+          <Text>🥚 Eggs</Text>
+          <Text className="font-bold">12</Text>
+        </View>
+
+        <View className="bg-white p-4 rounded-xl w-[30%]">
+          <Text>🍽️ Feed</Text>
+          <Text className="font-bold">7kg</Text>
+        </View>
+
+        <View className="bg-white p-4 rounded-xl w-[30%]">
+          <Text>🐔 Layers</Text>
+          <Text className="font-bold">17</Text>
+        </View>
+      </View>
+
     </View>
   );
 }
